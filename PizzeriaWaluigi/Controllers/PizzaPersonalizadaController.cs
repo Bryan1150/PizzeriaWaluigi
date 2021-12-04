cc033d90df982d7be1f8009c65de2b921ccbd7b9
Bryan1150 <bryan.umaa1150@gmail.com>
2021-12-03 22:26:55 -0600

    Controlador Pizza Personalizada Metodos Basicos

---
+++ PizzeriaWaluigi/Controllers/PizzaPersonalizadaController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PizzeriaWaluigi.Models;
+using PizzeriaWaluigi.Handlers;
+
+namespace PizzeriaWaluigi.Controllers
+{
+    public class PizzaPersonalizadaController : Controller
+    {
+        public ActionResult crearPizza()
+        {
+            IngredientesHandler accesoDatos = new IngredientesHandler();
+            ViewBag.ingredientes = accesoDatos.ObtenerTodosLosIngredientes();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult crearPizza(PizzaModel noticia)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //PizzaHandler accesoDatos = new NoticiasHandler();
+                    //ViewBag.ExitoAlCrear = accesoDatos.InsertarNoticia(noticia);
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "La noticia" + " " + noticia.Titulo + " fue creada con éxito :)";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salió mal y no fue posible crear la noticia :(";
+                return View();
+            }
+        }
+    }
+}
