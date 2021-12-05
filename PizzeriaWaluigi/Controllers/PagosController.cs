511b5ff2ef31ab168b5e58f23a8299fac65a79af
Bryan1150 <bryan.umaa1150@gmail.com>
2021-12-04 22:42:48 -0600

    Vista Datos de pago

---
+++ PizzeriaWaluigi/Controllers/PagosController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PizzeriaWaluigi.Models;
+using PizzeriaWaluigi.Handlers;
+
+namespace PizzeriaWaluigi.Controllers
+{
+    public class PagosController : Controller
+    {
+        // GET: Pagos
+        public ActionResult Envio_PagoDireccion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Envio_PagoDireccion(FacturaModel factura)
+        {
+            ActionResult view = RedirectToAction("Envio_PagoDatos", "Pagos", factura);
+            return View();
+        }
+
+        public ActionResult Envio_PagoDatos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Envio_PagoDatos(FacturaModel factura)
+        {
+
+            return View();
+        }
+    }
+}
