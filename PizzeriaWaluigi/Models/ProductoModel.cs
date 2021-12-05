f381b1e61ff8281458727ce86c5ffc9cdc5653c0
Bryan1150 <bryan.umaa1150@gmail.com>
2021-12-04 22:44:23 -0600

    Models modificados

--- PizzeriaWaluigi/Models/ProductoModel.cs
+++ PizzeriaWaluigi/Models/ProductoModel.cs
@@ -7,14 +7,14 @@ namespace PizzeriaWaluigi.Models
     public class ProductoModel
     {
         [Display(Name = "IdProducto")]
-        public string Id { get; set; }
+        public int Id { get; set; }
 
         [Display(Name = "Nombre")]
         [Required(ErrorMessage = "Es necesario que ingrese un nombre para el producto.")]
         public string Nombre { get; set; }
 
         [Display(Name = "Descripcion")]
-        public int Descripcion { get; set; }
+        public string Descripcion { get; set; }
 
         [Display(Name = "Precio")]
         [Required(ErrorMessage = "Es necesario que ingrese un precio para el producto.")]
---
+++ PizzeriaWaluigi/Models/TarjetaModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzeriaWaluigi.Models
+{
+    public class TarjetaModel
+    {
+        [Display(Name = "Numero de tarjeta")]
+        [Required(ErrorMessage = "Es necesario que ingrese un número")]
+        [MaxLength(16, ErrorMessage = "Debe ingresar 16 dígitos")]
+        [MinLength(16, ErrorMessage = "Debe ingresar 16 dígitos")]
+        public string NumeroTarjeta { get; set; }
+
+        [Display(Name = "Nombre del titular")]
+        [Required(ErrorMessage = "Es necesario que ingrese un nombre")]
+        public string NombreTarjeta { get; set; }
+
+        [Display(Name = "Fecha de expiracion")]
+        [Required(ErrorMessage = "Es necesario que ingrese una fecha")]
+        [MaxLength(6, ErrorMessage = "Máximo de 4 cáracteres")]
+        [MinLength(4, ErrorMessage = "Minimo de 4 cáracteres")]
+        public string FechaExpiracion { get; set; }
+
+        [Display(Name = "Codigo de seguridad")]
+        [Required(ErrorMessage = "Es necesario que ingrese un CVV")]
+        [MaxLength(4, ErrorMessage = "Máximo de 4 cáracteres")]
+        [MinLength(3, ErrorMessage = "Minimo de 4 cáracteres")]
+        public string CVV { get; set; }
+    }
+}
