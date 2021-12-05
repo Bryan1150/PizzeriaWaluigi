982abe77b9f58608537f0e2dc9e88006eab53243
Bryan1150 <bryan.umaa1150@gmail.com>
2021-12-04 22:43:44 -0600

    MenuController iniciado

---
+++ PizzeriaWaluigi/Controllers/MenuController.cs
@@ -0,0 +1,62 @@
+﻿using PizzeriaWaluigi.Handlers;
+using PizzeriaWaluigi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PizzeriaWaluigi.Controllers
+{
+    public class MenuController : Controller
+    {
+        public ActionResult Menu()
+        {
+            MenuHandler accesoDatos = new MenuHandler();
+            ViewBag.productosDisponibles = accesoDatos.ObtenerTodosLosProductos();
+            return View();
+        }
+
+        public ActionResult AgregarProducto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarProducto(ProductoModel producto)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    MenuHandler accesoDatos = new MenuHandler();
+                    ViewBag.ExitoAlCrear = accesoDatos.InsertarProducto(producto);
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El producto" + " " + producto.Nombre + " fue agregado a la base de datos.";
+                        ModelState.Clear();
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Algo salió mal y no fue posible agregar el producto.";
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salió mal y no fue posible agregar el producto.";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ObtenerImagen(string nombreIngrediente)
+        {
+            PizzaPersonalizadaHandler ingredienteHandler = new PizzaPersonalizadaHandler();
+            var tupla = ingredienteHandler.ObtenerFoto(nombreIngrediente);
+            return File(tupla.Item1, tupla.Item2);
+        }
+    }
+}
